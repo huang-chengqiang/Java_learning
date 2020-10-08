c123777c16ed9118250318ee11b7a843b3817679
huang-chengqiang <914978686@qq.com>
2020-10-08 23:05:48 +0800

    f

---
+++ code/book/chapter four/Example4_1.java
@@ -0,0 +1,15 @@
+class XiyoujiRenwu{
+    float height,weight;
+    String head,ear;
+    void speak(String s){
+        System.out.println(s);
+    }
+}
+public class Example4_1 {
+    public static void main(String[] args) {
+        XiyoujiRenwu zhubajie,sunwukong;
+        zhubajie = new XiyoujiRenwu();
+        sunwukong = new XiyoujiRenwu();
+    }
+
+}
