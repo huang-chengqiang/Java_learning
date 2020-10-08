00b5130631c06f75b2adbc30953a07337ce60fda
huang-chengqiang <914978686@qq.com>
2020-10-08 22:36:27 +0800

    a

---
+++ learning/数据类型/Text1.java
@@ -0,0 +1,45 @@
+public class Text1{
+	public static void  main(String[] args){
+		//System.out.print("sss\n");
+		//System.out.println("sss");
+		byte b = 1;           //整数类型
+		b = 2;
+		System.out.println(b);
+		
+		short c = 1;           //整数类型
+		c = 2;
+		System.out.println(c);
+		
+		int d = 1;           //整数类型
+		d = 2;
+		System.out.println(d);
+		
+		long e = 1l;           //整数类型
+		e = 2;
+		System.out.println(e);
+		
+		float f = 1.1f;           //浮点类型
+		f = 1.2f;
+		System.out.println(f);
+		
+				
+		double g = 1.2;           //浮点类型
+		g = 1.22;
+		System.out.println(g);
+		
+				
+		char h = '1';            //字符类型
+		h = '2';
+		System.out.println(h);
+		
+		boolean i = false;      //布尔类型
+		i = true;
+		System.out.println(i);
+		
+		//String str = "He";    //引用型，不可重新赋值
+		//str = "word";
+		String str1 = "hw";
+		System.out.println(str);
+		
+	}
+}
